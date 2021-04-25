21b734db9ce565bc3b10c6c0b43b8f5c18814d06
vijayEE1997 <vijay1997dhakad@gmail.com>
2021-04-25 23:01:57 +0530

    Patient_intake_project

---
+++ Patient_Appointment_JUnit5/src/module-info.java
@@ -0,0 +1,2 @@
+module jUnit5 {
+}
---
+++ Patient_Appointment_JUnit5/src/patientintake/ClinicMain.java
@@ -0,0 +1,79 @@
+package patientintake;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ClinicMain {
+	
+	public static ClinicCalender calender;
+	
+	public static void main(String[] args) throws Throwable {
+		
+		calender = new ClinicCalender();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Welcome to the Patient Intake Computer System! \n\n");
+		String lastOption ="";
+		while(!lastOption.equalsIgnoreCase("x")) {
+			lastOption = displayMenu(s);
+		}
+		
+		System.out.println("\nexiting System...\n");
+	}
+
+	private static String displayMenu(Scanner s) throws Throwable{
+		
+		System.out.println("Please Select an option:");
+		System.out.println("1. Enter a Patient Apppointment");
+		System.out.println("2. View All Appointments");
+		System.out.println("X. Exit System.");
+		System.out.println("Option: ");
+		
+		String option = s.next();
+		
+		switch(option) {
+		case "1" : performsPatientEntry(s);
+					return option;
+		case "2" : performAllAppointments(s);
+					return option;
+		default  : System.out.println("Invalid option, please re-enter."); 
+					return option;
+		}
+	}
+
+	private static void performAllAppointments(Scanner s) {
+		
+		System.out.println("\n\nAll Appointments in System");
+		
+		for(PatientAppointment p: calender.getAppointments()) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy hh:mm: a");
+			String apptTime = p.getAppointmentDateTime().format(formatter);
+			System.out.println(String.format("%s: %s, %s\t\tDoctor: %s",apptTime,p.getPatientLastName(),p.getPatientFirstName(),p.getDoctor().getName()));
+		}
+		
+		System.out.println("\nPress any key to continue...");
+	}
+
+	private static void performsPatientEntry(Scanner s) throws Throwable {
+
+		s.nextLine();
+		
+		System.out.println("\n\nPlease Enter Appointment Info:");
+		
+		System.out.println("Patient Last Name:");
+		String lastName = s.nextLine();
+		System.out.println("Patient First Name:");
+		String firstName = s.nextLine();
+		System.out.println("Appointment Date [M/d/yyyy h:m a]:");
+		String when = s.nextLine();
+		System.out.println("Doctor's Last Name:");
+		String doc = s.nextLine();
+		
+		try {
+			calender.addAppointment(firstName, lastName, doc, when);
+		}catch (Throwable e) {
+			System.out.println("Erros! "+ e.getMessage());
+			return;
+		}
+		
+	}
+}
